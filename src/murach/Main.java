6f483f410e870b020e45a6901f090d73f52f17a8
HaziqRidzwan <haziqridzwan@outlook.com>
2021-12-17 15:55:06 +0800

    Update 17.12.2021 2

---
+++ src/murach/Main.java
@@ -0,0 +1,55 @@
+//Calculate and display future value
+
+package murach;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// displayLine a welcome message
+		System.out.println("Welcome to the Future Value Calculator");
+		System.out.println();
+
+		Scanner sc = new Scanner(System.in);
+		String choice = "y"; // if y, program continues looping
+		while (choice.equalsIgnoreCase("y")) {
+
+			// get input from user
+			System.out.print("Enter monthly investment: ");
+			double monthlyInvestment = Double.parseDouble(sc.nextLine()); //parse means convert data to other format
+			System.out.print("Enter yearly interest rate: ");
+			double yearlyInterestRate = Double.parseDouble(sc.nextLine());
+			System.out.print("Enter number of years: ");
+			int years = Integer.parseInt(sc.nextLine());
+
+			// convert yearly values to monthly values
+			double monthlyInterestRate = yearlyInterestRate / 12 / 100;
+			int months = years * 12;
+
+			// calculate the future value
+			double futureValue = 0; //futureValue starts from zero
+			int i = 1;
+			while (i <= months) {
+				futureValue = futureValue + monthlyInvestment;
+				double monthlyInterestAmount = futureValue * monthlyInterestRate;
+				futureValue = futureValue + monthlyInterestAmount;
+				i = i + 1;
+			}
+
+			// format and display the result
+			System.out.println("Future value: " + NumberFormat.getCurrencyInstance().format(futureValue));
+			System.out.println();
+
+			// see if user wants to continue
+			System.out.print("Continue? (y/n): ");
+			choice = sc.nextLine();
+			System.out.println();
+		}
+		sc.close();
+		System.out.println("Bye!");
+	}
+
+}
